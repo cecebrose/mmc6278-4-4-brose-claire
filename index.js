a1478e5be036f7e07c48a3c9a14017c990c33095
cecebrose <clairebrose@ufl.edu>
2022-08-03 21:56:11 -0400

    all tests pass

--- index.js
+++ index.js
@@ -12,82 +12,80 @@ var words = [
   'mango'
 ]
 
-function onStart() {
-  updateDisplay();
-  document.addEventListener("keyup", onGuess);
-var currentWordIndex = 0;
-var attempts = 0;
-var guessedLetters = [];
+var selectedWord= ""
+var displayWord= ""
+var previousWord= ""
+var incorrectLetters= []
+var remainingGuesses= 10;
 var wins = 0;
 var losses = 0;
-};
 
-function onGuess(event) {
-  var keyPressed = event.key.toUpperCase();
-  console.log(keyPressed);
-
-
-  if (keyPressed){
-      if (guessedLetters.indexOf(keyPressed) === -1) {
-          guessedLetters.push(keyPressed);
-          attempts++;
-          updateDisplay();
-          if (isRoundLost()) {
-              losses++;
-              goToNextWord();
-              return;
-          } else if (isRoundWon()) {
-              wins++;
-              goToNextWord();
-              return;
-          }
-      }
-  }
-};
-
-function getWord() {
-  return (currentWordLetters().map(function (letter) {
-      if (guessedLetters.indexOf(letter) === -1) {
-          return "&nbsp";
-      } else {
-          return letter;
-      }
-  }
-  ));
-};
-
-function goToNextWord() {
-  currentWordIndex++;
-  attempts = 0;
-  guessedLetters = [];
+function startGame() {
+/* math.floor rounds DOWN to the nearest whole number & math.ceil rounds UP to the nearest whole number */
+/* math.round rounds either direction */
+  if (selectedWord){
+    previousWord=selectedWord
+    incorrectLetters=[]
+    remainingGuesses=10;
+  };
+  
+  var randomNum=Math.floor(Math.random()*11);
+  selectedWord=words[randomNum];
+  displayWord=selectedWord.replaceAll(/[\w]/g, "_") /* g=global */
   updateDisplay();
 };
-function isRoundLost() {
-  return (getGuessesAllowed() - attempts === 0);
-}
 
-function isRoundWon() {
-  return (getWordToGuess().indexOf("&nbsp") === -1);
-}
-function isLetterOnly(character) {
-  if (character.length !== 1) {
-      return false;
+function handleKeyUp(e){
+  var keyPressed = e.key.toLowerCase();
+  if (incorrectLetters.includes(keyPressed)||!/^[a-z]$/i.test(keyPressed))/* || = double pipes aka "or" */ {
+    return
   }
-  var checker = /^[A-Z]+$/i.test(character);
-  return checker;
+
+  if (selectedWord.includes(keyPressed)){
+    var matchingLocations=[]
+    for (var i=0; i<selectedWord.length; i++){
+      if (selectedWord[i]===keyPressed){
+        matchingLocations.push(i);
+      }
+    }
+
+    for (var j=0; j<matchingLocations.length; j++){
+      displayWord=`${displayWord.substring(0,matchingLocations[j])}${keyPressed}${displayWord.substring(matchingLocations[j]+1)}`
+    }
+
+  }
+  else {
+    incorrectLetters.push(keyPressed)
+    remainingGuesses --
+  }
+  isGameOver()
+  updateDisplay()
+};
+
+function isGameOver(){
+var gameOver=false;
+if (!displayWord.includes("_")){
+  wins++ 
+  gameOver=true;
 }
+if (remainingGuesses===0){
+  losses++
+  gameOver=true;
+}
+if (gameOver){
+  startGame()
+}
+};
+
 function updateDisplay() {
-  document.getElementById("word-to-guess").innerHTML = guessedLetters.reduce(function (list, letter) {
-      return (list + letter + " ");
-  }, "");
-  showGameBoard();
-  document.getElementById("previous-word").innerHTML = showPreviousWord() - attempts;
+  console.log("updating");
+  document.getElementById("word-to-guess").innerHTML = displayWord;
+  document.getElementById("previous-word").innerHTML = previousWord;
   document.getElementById("wins").innerHTML = wins;
   document.getElementById("losses").innerHTML = losses;
-  document.getElementById("score").innerHTML = score;
-  document.getElementById("remaining-guesses").innerHTML = getGuessesAllowed(10) - attempts;
+  document.getElementById("incorrect-letters").innerHTML = incorrectLetters;
+  document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
 };
 
-function currentWordLetters() {
-  return (words[currentWordIndex].split(""));
-};
+document.onkeyup=handleKeyUp;
+document.onload=startGame();
